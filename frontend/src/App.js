158c6203d158f703a53e02e997802eeaa210b8a4
Sanskar Vinodkumar Bajaj <131588500+SanskarBajaj123@users.noreply.github.com>
2025-04-12 12:34:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -71,7 +71,7 @@ function App() {
 
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/predict', {
+      const response = await fetch('https://emotion-analyzer-sr1i.onrender.com/api/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
